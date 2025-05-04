d8ae2228478288b1c22b8a691284430c348e067d
Arun Sisarran <arunsisarran@gmail.com>
2025-05-04 11:16:17 -0400

    added suspense wrapper to make it vercel compliant

--- fuelu/app/recipe/page.js
+++ fuelu/app/recipe/page.js
@@ -1,8 +1,21 @@
+import { Suspense } from "react"
 import RecipePage from "../pages/RecipePage"
 
 export default function Recipe() {
   return (
-    <RecipePage />
+    <Suspense fallback={<LoadingFallback />}>
+      <RecipePage />
+    </Suspense>
   )
 }
 
+function LoadingFallback() {
+  return (
+    <div className="relative min-h-screen">
+      <div className="absolute inset-0 bg-black/50 z-10" />
+      <div className="relative z-20 flex justify-center items-center min-h-screen">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-[#d1b2a1]"></div>
+      </div>
+    </div>
+  )
+}
